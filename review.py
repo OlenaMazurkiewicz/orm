0346002f2db4497461c34d760e6b72ec9c8cf79f
Olena Mazurkiewicz <58334662+OlenaMazurkiewicz@users.noreply.github.com>
2020-12-03 15:58:02 +0200

    Update review.py

--- review.py
+++ review.py
@@ -8,6 +8,8 @@ class Review:
         self.customer = customer
         self.score = int(score)
         self.status = 'Moderation'
+        self.log = logger
+        self.log.info(f'New review of {self.customer} was created)
 
     def __str__(self):
         return f"Review {self.customer}: score {self.score}, {self.text}"
